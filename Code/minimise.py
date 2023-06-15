2567c49be053f6f381b4a29b497b9c17ab1458ee
Adele-Desmazieres <adele.desmazi@gmail.com>
2023-06-15 10:26:04 +0200

    Indexing of fasta file instead of loading it in memory

--- Code/minimise.py
+++ Code/minimise.py
@@ -4,26 +4,33 @@ import subprocess
 import shutil
 import argparse
 
-TMP_FILENAME = "tmp.fasta"
-
-
+TMPFILE = "tmp.fasta"
+INPUTFILE = ""
+CMD = ""
 
 # writes the sequences and their species in a fasta file
-def seqs_to_file(seqs, filename) :
+def iseqs_to_file(iseqs, filename) :
+	finput = open(INPUTFILE, 'r')
 	f2 = open(filename, 'w')
 
-	for index,(header,seq) in enumerate(seqs.items()) :
+	for (index, (specie, (begin, end))) in enumerate(iseqs.items()) :
 		if index != 0 :
 			f2.write("\n")
-		f2.write(">" + str(header[0]) + ", position " + str(header[1]) + "\n" + seq)
+		s = specie
+		s = specie.rstrip('#$') + " " + str(begin)
+		f2.write(">" + s + "\n")
+		finput.seek(begin)
+		s = finput.read(end-begin)
+		f2.write(s)
 	
+	finput.close()
 	f2.close()
 
 
 # check that the execution of cmd with the sequences as input gives the desired output
 # TODO : execute on the cluster
-def check_output(seqs, cmd, desired_output, wd) :
-	seqs_to_file(seqs, TMP_FILENAME)
+def check_output(iseqs, cmd, desired_output, wd) :
+	iseqs_to_file(iseqs, TMPFILE)
 	output = subprocess.run(cmd, shell=True, capture_output=True, cwd=wd)
 
 	#print("Exe output : " + str((output.returncode, output.stdout, output.stderr)))
@@ -31,159 +38,155 @@ def check_output(seqs, cmd, desired_output, wd) :
 	checkreturn = desired_output[0] == None or desired_output[0] == output.returncode
 	checkstdout = desired_output[1] == None or desired_output[1] == output.stdout
 	checkstderr = desired_output[2] == None or desired_output[2] == output.stderr
+	r = (checkreturn and checkstdout and checkstderr)
 
-	return (checkreturn and checkstdout and checkstderr)
+	return r
 
 
 # reduces the sequence by cutting begining or ending nucleotides
 # cutting in half successively, with an iterative dichotomy
 # returns the new reduced sequence inside the dict sequences, and its new position
-def strip_sequence(seqs, sp_loc, cmd, desired_output, wd, flag_begining) :
-	seq = seqs[sp_loc]
-	seq_ret = seq # the result when it will be reduced
-	loc_ret = sp_loc[1]
-	tmp_seqs = seqs.copy()
+def strip_sequence(iseqs, specie, cmd, desired_output, wd, flag_begining) :
+	(begin, end) = iseqs[specie]
 	found = False
 	
 	if flag_begining :
-		imin = 0
-		imax = len(seq)//2
+		imin = begin
+		imax = (begin+end) // 2
 	else :
-		imin = len(seq)//2
-		imax = len(seq)
+		imin = (begin+end) // 2
+		imax = end
 
 	while not found :
 
 		imid = (imin + imax) // 2
+
 		# stopping condition
 		if imid == imin or imid == imax :
+			loc = (imin, end) if flag_begining else (begin, imax)
+			iseqs[specie] = loc
 			found = True
+			continue
 
-		#print(seq, imin, imid, imax)
-
-		seq1 = seq[imid:] if flag_begining else seq[:imid]
-		tmp_seqs[sp_loc] = seq1
-
-		# the cut maintained the output, we keep cutting toward the center of the sequence
-		if check_output(tmp_seqs, cmd, desired_output, wd) :
-			seq_ret = seq1
+		# get the most central quarter
+		loc = (imid, end) if flag_begining else (begin, imid)
+		iseqs[specie] = loc
+		# if the cut maintain the output, we keep cutting toward the center of the sequence
+		if check_output(iseqs, cmd, desired_output, wd) :
 			if flag_begining :
 				imin = imid
-				loc_ret = sp_loc[1] + imid
 			else :
 				imax = imid
 
-		# else the cut didn't maintain the output, we keep cutting toward the exterior
+		# else the cut doesn't maintain the output, so we keep cutting toward the exterior
 		else :
-			tmp_seqs[sp_loc] = seq
 			if flag_begining :
 				imax = imid
 			else :
 				imin = imid
 	
-	# we delete the old sequence from the dict, and add the new one with its new position
-	del seqs[sp_loc]
-	seqs[(sp_loc[0], loc_ret)] = seq_ret
-	return seqs, loc_ret
+	return iseqs
 
 
 # returns the new sequences dict with the reduced sequenced of the specified specie
 # use an iterative dichotomy
-def dichotomy_cut_one_seq_iter(seqs, sp_loc, cmd, desired_output, wd) : 
+def dichotomy_cut_one_seq_iter(iseqs, specie, cmd, desired_output, wd) : 
 
-	specie = sp_loc[0]
-	location = sp_loc[1]
-	location_tmp = location
 	found = False
 
 	while not found :
-		#print(seqs)
+		(begin, end) = iseqs[specie]
+		middle = (begin + end) // 2
 
-		seq = seqs[(specie, location)]
+		#seq = seqs[(specie, location)]
 
 		# case where the target fragment is in the first half
-		seq0 = seq[:len(seq)//2]
-		seqs[(specie, location)] = seq0
-		if seq0 != seq and check_output(seqs, cmd, desired_output, wd) :
+		#seq0 = seq[:len(seq)//2]
+		#seqs[(specie, location)] = seq0
+		iseqs[specie] = (begin, middle)
+		if middle != end and check_output(iseqs, cmd, desired_output, wd) :
 			continue
 		else :
-			del seqs[(specie, location)]
+			del iseqs[specie]
 
 		# case where the target fragment is in the second half
-		seq1 = seq[len(seq)//2:]
-		location_tmp += len(seq)//2
-		seqs[(specie, location_tmp)] = seq1
-		if seq1 != seq and check_output(seqs, cmd, desired_output, wd) :
-			location = location_tmp
+		#seq1 = seq[len(seq)//2:]
+		#location_tmp += len(seq)//2
+		#seqs[(specie, location_tmp)] = seq1
+		iseqs[specie] = (middle, end)
+		if middle != end and check_output(iseqs, cmd, desired_output, wd) :
 			continue
 		else :
-			del seqs[(specie, location_tmp)]
+			del iseqs[specie]
 
-		# case where there is two sequences in co-factor in the sequence
+		# case where there are two co-factor sequences
 		# so we separate them under two different header
-		sp0 = (specie, location)
-		sp1 = (specie, location + len(seq)//2)
-		seqs[sp0] = seq0
-		seqs[sp1] = seq1
-		if check_output(seqs, cmd, desired_output, wd) :
-			seqs = dichotomy_cut_one_seq_iter(seqs, sp0, cmd, desired_output, wd)
-			seqs = dichotomy_cut_one_seq_iter(seqs, sp1, cmd, desired_output, wd)
+		sp0 = specie + "#"
+		sp1 = specie + "$"
+		iseqs[sp0] = (begin, middle)
+		iseqs[sp1] = (middle, end)
+		# TODO : relancer le check_output pour chopper les co-factors qui ne sont pas de part et d'autre du milieu de la séquence
+		if check_output(iseqs, cmd, desired_output, wd) :
+			iseqs = dichotomy_cut_one_seq_iter(iseqs, sp0, cmd, desired_output, wd)
+			iseqs = dichotomy_cut_one_seq_iter(iseqs, sp1, cmd, desired_output, wd)
 			break
 		else : 
-			del seqs[sp0]
-			del seqs[sp1]
+			del iseqs[sp0]
+			del iseqs[sp1]
 
 		# case where the target sequence is on both sides of the cut
 		# so we strip first and last unnecessary nucleotids
-		seqs[(specie, location)] = seq
-		seqs, location = strip_sequence(seqs, (specie, location), cmd, desired_output, wd, True)
-		seqs, location = strip_sequence(seqs, (specie, location), cmd, desired_output, wd, False)
+		iseqs[specie] = (begin, end)
+		iseqs = strip_sequence(iseqs, specie, cmd, desired_output, wd, True)
+		iseqs = strip_sequence(iseqs, specie, cmd, desired_output, wd, False)
 		found = True
 	
-	return seqs
+	return iseqs
 
 
 # returns every reduced sequences
-def dichotomy_cut(seqs, cmd, desired_output, wd) :
-	cutted_seqs = seqs.copy()
-
-	for sp_loc,seq in seqs.items() :
-		#print("\n", sp_loc)
+def dichotomy_cut(iseqs, cmd, desired_output, wd) :
+	cutted_iseqs = iseqs.copy()
+	for sp in iseqs.keys() :
 
 		# check if desired output is obtained whithout the sequence of the specie
-		tmp_seqs = {k:v for k,v in cutted_seqs.items() if k != sp_loc}
-		if check_output(tmp_seqs, cmd, desired_output, wd) :
-			cutted_seqs = tmp_seqs
-			#print(cutted_seqs)
+		tmp_iseqs = {k:v for k,v in cutted_iseqs.items() if k != sp}
+		if check_output(tmp_iseqs, cmd, desired_output, wd) :
+			cutted_iseqs = tmp_iseqs
 		
 		# otherwise reduces the sequence
 		else :
-			cutted_seqs = dichotomy_cut_one_seq_iter(cutted_seqs, sp_loc, cmd, desired_output, wd)
-		
-	return cutted_seqs
+			cutted_iseqs = dichotomy_cut_one_seq_iter(cutted_iseqs, sp, cmd, desired_output, wd)
+
+	print(cutted_iseqs)
+	return cutted_iseqs
 
 
-# returns the representation of a fasta file by a dictionnary where
-# the key is the specie header and the location of the sequence
-# and the value is the specie sequence
-# for example : {("Fraise",0):"ATTCG", ("Pomme",0):"GGGGCTC"}
+# returns the representation of a fasta file parsed in a dictionnary where
+# the key is the specie header
+# and the value is the tuple (begin, end) 
+# they are the index of the characteres of the sequence in the file
+# "begin" is included and "end" is excluded
+# for example : {"Fraise":(8, 25), "Pomme":(33, 50)}
 def parsing(filename) :
 	try :
 		with open(filename, 'r') as f :
 			specie = None
 			sequences = dict()
+			c = 0
 
 			for line in f :
-				line = line.rstrip('\n')
-				if len(line) >= 1 : 
+				c += len(line)
+				if line[0] == '>' :
 
-					if line[0] == '>' :
-						specie = line[1:]
-						sequences[(specie, 1)] = ""
+					if specie != None :
+						sequences[specie] = (sequences[specie][0], c-len(line)-1)
 
-					elif specie != None : 
-						sequences[(specie, 1)] += line # concatenate line to the previous string
+					#specie = line[1:].rstrip('\n') + ", position 1"
+					specie = line[1:].rstrip('\n')
+					sequences[specie] = (c, c+1)
 
+		sequences[specie] = (sequences[specie][0], c)
 		return sequences
 
 	except IOError :
@@ -191,19 +194,19 @@ def parsing(filename) :
 
 
 def rm_tmpfile() :
-	os.remove(TMP_FILENAME)
+	os.remove(TMPFILE)
 
 
 def init_tmpfile(inputfile) :
 	try :
-		with open(TMP_FILENAME, 'x') :
-			shutil.copy(inputfile, TMP_FILENAME)
+		with open(TMPFILE, 'x') :
+			shutil.copy(inputfile, TMPFILE)
 	except OSError :
-		print(TMP_FILENAME + " already exists, unable to create it.")
-		truncate = input("Do you want to truncate " + TMP_FILENAME + " ? (y,n) ")
+		print(TMPFILE + " already exists, unable to create it.")
+		truncate = input("Do you want to truncate " + TMPFILE + " ? (y,n) ")
 		if truncate == 'y' : 
-			with open(TMP_FILENAME, 'w') as tmp :
-				shutil.copy(inputfile, TMP_FILENAME)
+			with open(TMPFILE, 'w') as tmp :
+				shutil.copy(inputfile, TMPFILE)
 		else :
 			exit(0)
 
@@ -239,23 +242,25 @@ if __name__=='__main__' :
 	desired_output = (args.returncode, args.stdout, args.stderr)
 	wd = args.workdir if args.workdir[len(args.workdir)-1] != '/' else args.workdir[:-1]
 	outputfile = get_outputfile(args.destfile, wd)
-	TMP_FILENAME = wd + "/" + TMP_FILENAME
+	TMPFILE = wd + "/" + TMPFILE
+
 	init_tmpfile(args.filename)
 
 	if args.verbose :
 		print("Desired output : " + str(desired_output))
 		print("Working directory : " + wd)
-		print("Tmp filename : " + TMP_FILENAME)
+		print("Tmp filename : " + TMPFILE)
 		print("Output filename : " + outputfile + "\n")
 
-	seqs = parsing(args.filename)
-	cutted_seqs = dichotomy_cut(seqs, args.cmdline, desired_output, wd)
+	INPUTFILE = args.filename
+	iseqs = parsing(INPUTFILE)
+	cutted_iseqs = dichotomy_cut(iseqs, args.cmdline, desired_output, wd)
 
-	seqs_to_file(cutted_seqs, outputfile)
+	iseqs_to_file(cutted_iseqs, outputfile)
 	rm_tmpfile()
 
 	if args.verbose :
-		print("Minimised sequences : \n")
+		print("\nMinimised sequences:")
 		with open(outputfile) as f :
 			print(f.read())
 		print("\nDone.")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project : Genome Fuzzing
 
-Status : in developpment
+*Status : in developpment*
 
 ## Description
 
@@ -8,19 +8,27 @@ This is a bioinformatic tool that founds the minimal input for a specified comma
 
 ## Installation
 
+Download the code from [Github](https://github.com/Adele-Desmazieres/Pasteur-Genome-Fuzzing). 
 
+Python3 is required. 
 
 ## Usage
 
 In your console from the Code directory you can run : 
 
 ```sh
-$ python3 minimiser.py <path/to/inputfile.fasta> <"command line with tmp.fasta instead of the path/to/inputfile.fasta"> <value of desired returncode>
+$ python3 minimiser.py 
+    <path/to/inputfile.fasta> 
+    <"command line with tmp.fasta instead of the path/to/inputfile.fasta"> 
+    <value of desired returncode>
 ```
 
 For example : 
 ```sh
-$ python3 minimise.py ../Data/example.fasta "python3 ../Data/executable.py tmp.fasta" 1
+$ python3 minimise.py 
+    ../Data/example.fasta 
+    "python3 ../Data/executable.py tmp.fasta" 
+    1
 ```
 
 To print the options of the program you can run :
@@ -30,17 +38,29 @@ $ python3 minimise.py -h
 
 You can specify the directory where the given command line should be run with the option -w. If not specified, it will be executed in the current directory. For example, from the Code directory :
 ```sh
-$ python3 minimise.py -w ../Data ../Data/example.fasta "python3 executable.py tmp.fasta" 1
+$ python3 minimise.py 
+    -w ../Data 
+    ../Data/example.fasta 
+    "python3 executable.py tmp.fasta" 
+    1
 ```
 
 You can specify the name and destination of the output file with the option -d. If not specified, the output file will be created in the working directory with the name "minimised.fasta". The output file will truncate any existing file at same path with same name. For example, from the Code directory :
 ```sh
-$ python3 minimise.py -d ../Results/minimised.fasta ../Data/example.fasta "python3 ../Data/executable.py tmp.fasta" 1
+$ python3 minimise.py 
+    -d ../Results/minimised.fasta 
+    ../Data/example.fasta 
+    "python3 ../Data/executable.py tmp.fasta" 
+    1
 ```
 
 You can also run the program from any directory and combine multiples options, like so from the root of this project :
 ```sh
-$ python3 Code/minimise.py -d Results/minimised.fasta -w Tests Tests/t2.fasta "python3 e1.py tmp.fasta" 1 -v
+$ python3 Code/minimise.py -v
+    -d Results/minimised.fasta 
+    -w Tests Tests/t2.fasta 
+    "python3 e1.py tmp.fasta" 
+    1
 ```
 
 
@@ -52,4 +72,6 @@ Contact : adesmaz.pro@gmail.com
 
 ## Licence
 
-GNU AFFERO, GPL3 (licence change request : adesmaz.pro@gmail.com)
+GNU AFFERO, GPL3
+
+Licence change request : adesmaz.pro@gmail.com
