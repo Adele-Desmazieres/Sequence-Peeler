748ace41a7871ce9c9dc76111467d03fac5a1b3e
Adele-Desmazieres <adele.desmazi@gmail.com>
2023-06-21 14:10:41 +0200

    Settled on the method to handle File of Files

--- Code/minimise.py
+++ Code/minimise.py
@@ -1,11 +1,14 @@
 import os
 import os.path
+from pathlib import Path
 import subprocess
 import shutil
 import argparse
 
-TMPFILE = "tmp.fasta"
-INPUTFILE = ""
+#TMPFILE = "tmp.fasta"
+TMP_EXTENSION = "_tmp"
+INFILESNAMES = []
+FOFNAME = ""
 CMD = ""
 WORKDIR = ""
 DESIRED_OUTPUT = None
@@ -33,7 +36,7 @@ def printset(iseqs) :
 
 # writes the sequences and their species in a fasta file
 def iseqs_to_file(iseqs, filename) :
-	finput = open(INPUTFILE, 'r')
+	finput = open(None, 'r') # TODO : remplacer None
 	f2 = open(filename, 'w')
 
 	ordered_iseqs = sorted(list(iseqs), key=lambda x:x.begin_seq) # alphabetical ordering of header's sequences
@@ -58,10 +61,20 @@ def iseqs_to_file(iseqs, filename) :
 	f2.close()
 
 
+def sp_to_files(spbyfile) :
+
+	for iseqs in spbyfile :
+
+		tmp_specie = iseqs.pop()
+		filename = tmp_specie.filename
+		iseqs.add(tmp_specie)
+		iseqs_to_file(iseqs, filename)
+
+
 # check that the execution of cmd with the sequences as input gives the desired output
 # TODO : execute on the cluster
-def check_output(iseqs) :
-	iseqs_to_file(iseqs, TMPFILE)
+def check_output(spbyfile) :
+	sp_to_files(spbyfile)
 	output = subprocess.run(CMD, shell=True, capture_output=True, cwd=WORKDIR)
 
 	checkreturn = DESIRED_OUTPUT[0] == None or DESIRED_OUTPUT[0] == output.returncode
@@ -75,7 +88,7 @@ def check_output(iseqs) :
 # reduces the sequence, cutting first and last nucleotides
 # cutting in half successively with an iterative binary search
 # returns the new reduced sequence, and adds it to the species's list of seqs
-def strip_sequence(seq, sp, iseqs, flag_begining) :
+def strip_sequence(seq, sp, iseqs, spbyfile, flag_begining) :
 	
 	(begin, end) = seq
 	seq1 = seq
@@ -90,7 +103,7 @@ def strip_sequence(seq, sp, iseqs, flag_begining) :
 		# get the most central quarter
 		seq1 = (imid, end) if flag_begining else (begin, imid)
 		sp.subseqs.add(seq1)
-		r = check_output(iseqs)
+		r = check_output(spbyfile)
 		sp.subseqs.remove(seq1)
 
 		# if the cut maintain the output, we keep cutting toward the center of the sequence
@@ -117,7 +130,7 @@ def strip_sequence(seq, sp, iseqs, flag_begining) :
 
 # reduces the sequences of the specie and return the new set of SpecieData instances
 # use an iterative binary search
-def reduce_specie(sp, iseqs) :
+def reduce_specie(sp, iseqs, spbyfile) :
 	
 	tmpsubseqs = sp.subseqs.copy()
 	
@@ -132,7 +145,7 @@ def reduce_specie(sp, iseqs) :
 		
 		# case where the target fragment is in the first half
 		sp.subseqs.add(seq1)
-		if middle != end and check_output(iseqs) :
+		if middle != end and check_output(spbyfile) :
 			tmpsubseqs.add(seq1)
 			continue
 		else :
@@ -140,7 +153,7 @@ def reduce_specie(sp, iseqs) :
 		
 		# case where the target fragment is in the second half
 		sp.subseqs.add(seq2)
-		if middle != begin and check_output(iseqs) :
+		if middle != begin and check_output(spbyfile) :
 			tmpsubseqs.add(seq2)
 			continue
 		else :
@@ -151,7 +164,7 @@ def reduce_specie(sp, iseqs) :
 		# TODO : relancer le check_output pour trouver les co-factors qui ne sont pas de part et d'autre du milieu de la séquence
 		sp.subseqs.add(seq1)
 		sp.subseqs.add(seq2)
-		if middle != end and middle != begin and check_output(iseqs) :
+		if middle != end and middle != begin and check_output(spbyfile) :
 			tmpsubseqs.add(seq1)
 			tmpsubseqs.add(seq2)
 			continue
@@ -162,43 +175,41 @@ def reduce_specie(sp, iseqs) :
 		# case where the target sequence is on both sides of the cut
 		# so we strip first and last unnecessary nucleotids
 		sp.subseqs.add(seq)
-		seq = strip_sequence(seq, sp, iseqs, True)
-		seq = strip_sequence(seq, sp, iseqs, False)
+		seq = strip_sequence(seq, sp, iseqs, spbyfile, True)
+		seq = strip_sequence(seq, sp, iseqs, spbyfile, False)
 	
 	return iseqs	
 
 
-# returns every reduced sequences in a set of SpecieData
-def reduce_one_file(iseqs) :
-	cutted_iseqs = iseqs.copy()
+# returns every reduced sequences of a file in a set of SpecieData
+def reduce_one_file(iseqs, spbyfile) :
+	reduced_iseqs = iseqs.copy()
 
 	for sp in iseqs :
 		# check if desired output is obtained whithout the sequence of the specie
-		cutted_iseqs.remove(sp)
+		reduced_iseqs.remove(sp)
 		
-		if not check_output(cutted_iseqs) :
+		if not check_output(spbyfile) :
 			# otherwise reduces the sequence
-			cutted_iseqs.add(sp)
-			cutted_iseqs = reduce_specie(sp, cutted_iseqs)
+			reduced_iseqs.add(sp)
+			reduced_iseqs = reduce_specie(sp, reduced_iseqs, spbyfile)
 
-	return cutted_iseqs
+	return reduced_iseqs
 
 
-'''
-def reduce_all_files(seqsbyfile) :
-	reduced = seqsbyfile.copy()
+def reduce_all_files(spbyfile) :
+	reduced = spbyfile.copy()
 
-	for iseqs in seqsbyfile :
+	for iseqs in spbyfile :
 		# check if desired output is obtained whithout the sequences of the file
-		reduced.remove(seqsbyfile)
+		reduced.remove(iseqs)
 		
 		if not check_output(reduced) :
 			# otherwise reduces the sequences of the file
 			reduced.add(iseqs)
-			reduced = reduce_one_file(iseqs, cutted_iseqs)
+			reduced = reduce_one_file(iseqs, reduced)
 
 	return reduced
-'''
 
 
 # returns the representation of a fasta file parsed in a set of SpecieData
@@ -242,72 +253,101 @@ def parsing(filename) :
 
 # takes a file that contains the files name
 # and return the set of sets of species by file
-def parsing_multiple_files(fofname) :
+def parsing_multiple_files(filesnames) :
+	spbyfile = set()
+
+	for filename in filesnames :
+		if len(filename) >= 1 :
+			seqs = parsing(filename)
+			spbyfile.add(seqs)
+
+	return spbyfile
+
+
+# removes the file TMPFILE
+def rm_tmpfiles(filesnames) :
+
+	for fname in filesnames : 
+		p = Path(fname)
+		tmp_fname = str(p.parent) + p.stem + TMP_EXTENSION + p.suffix
+		Path(tmp_fname).rename(Path(fname)) # rename the original files with their original name, and truncate the temporary files
+
+
+# copies a file with the same name with an extension to its stem
+# ask the user to truncate it when it already exists
+# raise an error if a directory with its name already exists
+def copy_file_with_extension(filename, extension) :
+	p = Path(filename)
+	tmp_fname = str(p.parent) + p.stem + extension + p.suffix
+
+	try :
+		with open(tmp_fname, 'x') : # exclusive creation
+			shutil.copy(filename, tmp_fname)
+
+	except OSError :
+
+		if Path(tmp_fname).isfile() :
+			print(tmp_fname + " file already exists, unable to create it.")
+			truncate = input("Do you want to truncate " + tmp_fname + " ? (y,n) ")
+			if truncate == 'y' : 
+				shutil.copy(filename, tmp_fname)
+			else :
+				exit(0)
+		
+		else :
+			print(tmp_fname + " directory already exists, unable to create it.")
+			raise
+
+
+# creates the temporary files as copies of input files
+def copy_infiles(filesnames) :
+	for filename in filesnames : 
+		copy_file_with_extension(filename, TMP_EXTENSION)
+
+def copy_fof(fofname) :
+	copy_file_with_extension(fofname, TMP_EXTENSION)	
+
+
+# returns the list of filenames in the fof (file of files)
+def fof_to_list(fofname) :
 	try :
 		with open(fofname) as fof :
-			seqsbyfile = set()
+			filesnames = []
 
 			for line in fof :
-
 				filename = line.rstrip('\n')
-				seqs = parsing(filename)
-				seqsbyfile.add(seqs)
+				if len(filename) >= 1 :
+					filesnames.append(filename)
 
-		return seqsbyfile
+		return filesnames
 
 	except IOError :
 		print("File " + fofname + " not found.")
 		raise
 
 
-# removes the file TMPFILE
-def rm_tmpfile() :
-	os.remove(TMPFILE)
-
-
-# creates the file TMP which is a copy of the inputfile
-# ask the user to truncate it if the file already exists
-# raise an error if a directory with this name already exists
-def init_tmpfile(inputfile) :
-	try :
-		with open(TMPFILE, 'x') :
-			shutil.copy(inputfile, TMPFILE)
-
-	except OSError :
-
-		if os.path.isfile(TMPFILE) :
-			print(TMPFILE + " already exists, unable to create it.")
-			truncate = input("Do you want to truncate " + TMPFILE + " ? (y,n) ")
-			if truncate == 'y' : 
-				shutil.copy(inputfile, TMPFILE)
-			else :
-				exit(0)
-		
-		else :
-			print("Unable to create the file" + TMPFILE + " : a directory with this name already exists.")
-			raise
-
-
 # set the destination file to "workdir/minimised.fasta" if None specified
-def get_outputfile(destfile, workdir) :
-	if destfile == None :
-		destfile = workdir + "/minimised.fasta"
-	return destfile
+def get_destdir(destdir, workdir) :
+	if destdir == None :
+		destdir = workdir
+	destdir = destdir if destdir[len(destdir)-1] == '/' else destdir + '/'
+	return destdir
 
 
 def get_args() :
 	parser = argparse.ArgumentParser(prog="Genome Fuzzing")
 
 	# options that takes a value
-	parser.add_argument('-d', '--destfile', default=None)
+	parser.add_argument('-d', '--destdir', default=None)
 	parser.add_argument('-e', '--stderr', default=None)
+	#parser.add_argument('-f', '--onefasta', action='store_true')
 	parser.add_argument('-o', '--stdout', default=None)
 	parser.add_argument('-r', '--returncode', default=None, type=int)
 	parser.add_argument('-v', '--verbose', action='store_true')
 	parser.add_argument('-w', '--workdir', default=os.getcwd())
 
 	# positionnal arguments
-	parser.add_argument('filename')
+	parser.add_argument('file_of_files_name')
 	parser.add_argument('cmdline')
 	
 	args = parser.parse_args()
@@ -324,33 +364,40 @@ if __name__=='__main__' :
 	DESIRED_OUTPUT = (args.returncode, args.stdout, args.stderr)
 	CMD = args.cmdline
 
-	# initialise the globals and the directories
-	WORKDIR = args.workdir if args.workdir[len(args.workdir)-1] != '/' else args.workdir[:-1]
-	outputfile = get_outputfile(args.destfile, WORKDIR)
-	INPUTFILE = args.filename
-	TMPFILE = WORKDIR + "/" + TMPFILE
-	init_tmpfile(args.filename)
-	# from here every global stays constant
+	# initialise the globals
+	WORKDIR = args.workdir if args.workdir[len(args.workdir)-1] == "/" else args.workdir + "/"
+	destdir = get_destdir(args.destdir, WORKDIR)
+	FOFNAME = args.file_of_files_name
+	INFILESNAMES = fof_to_list(FOFNAME)
+	# from here every global should be constant
+
+	# copies the input files in temporary files, not to overwrite the temporary ones
+	copy_infiles(INFILESNAMES)
+	copy_fof(FOFNAME)
 
 	if args.verbose :
 		print()
 		print(" - Desired output : " + str(DESIRED_OUTPUT))
 		print(" - Working directory : " + WORKDIR)
-		print(" - Tmp filename : " + TMPFILE)
-		print(" - Output filename : " + outputfile + "\n")
+		print(" - Fofname : " + FOFNAME)
+		print(" - Input files names : " + INFILESNAMES)
+		print(" - Output directory : " + destdir + "\n")
 
-	iseqs = parsing(INPUTFILE)
+	spbyfile = parsing_multiple_files(None) # TODO : remplacer None
 
 	# process the data
-	cutted_iseqs = reduce_one_file(iseqs)
+	reduced_spbyfile = reduce_all_files(spbyfile)
 
-	iseqs_to_file(cutted_iseqs, outputfile)
-	rm_tmpfile()
+	# writes the reduced seqs in files in destination directory
+	sp_to_files(reduced_spbyfile, destdir)
 
-	if args.verbose :
-		print("\n - Minimised sequences:")
-		with open(outputfile) as f :
-			print(f.read())
-		print("\nDone.")
+	# removes the temporary files and rename the original ones
+	rm_tmpfiles(INFILESNAMES)
+
+	#if args.verbose :
+	#	print("\n - Minimised sequences:")
+	#	with open(outputfile) as f :
+	#		print(f.read())
+	#	print("\nDone.")
 
 	
